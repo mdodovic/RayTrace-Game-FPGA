f50e64c4662af7f37bc90524d4569e247e360721
mihailopacaric <56758141+mihailopacaric@users.noreply.github.com>
2019-10-26 12:10:28 +0200

    BasicInputs
    W,S: forward, backward;
    Arrows: looking around;

--- Source.cpp
+++ Source.cpp
@@ -63,14 +63,47 @@ public:
 	vector3 operator-(vector3 p) {
 		return vector3(x - p.x, y - p.y, z - p.z);
 	}
+	vector3 operator*(float p) {
+		return vector3(x*p, y*p, z*p);
+	}
+	friend vector3 operator*(vector3 x, vector3 y);
+	vector3& operator+=(vector3 p) {
+		x += p.x;
+		y += p.y;
+		z += p.z;
+		return *this;
+	}
+	vector3& operator-=(vector3 p) {
+		x -= p.x;
+		y -= p.y;
+		z -= p.z;
+		return *this;
+	}
+	vector3 operator/(float p) {
+		return vector3(x/p, y/p, z/p);
+	}
+	vector3 operator-() {
+		return vector3(-x,-y,-z);
+	}
 	vector3 operator+(vector3 p) {
 		return vector3(x + p.x, y + p.y, z + p.z);
 	}
-
+	vector3 normalize();
 	float abs();
+
+	void ispis() { cout << "(" << x << " ," << y << " ," << z << ")" << endl; };
 
 	static vector3 toDot(vector3 pos, vector3 dir, float t);
 };
+
+vector3 operator*(vector3 x, vector3 y) {
+	return vector3(x.y*y.z-x.z*y.y, x.z*y.x-x.x*y.z, x.x*y.y-x.y*y.x);
+}
+
+vector3 vector3::normalize() {
+	float len = this->abs();
+	return (*this) / len;
+}
 
 float vector3::abs() {
 	return sqrt(rt(x) + rt(y) + rt(z));
@@ -247,14 +280,14 @@ float GetCounter()
 void multiThread(camera cam, float sinOfF, float pixelRatio, float piyelRatio, float RATIO, int lowx, int lowy, int highx, int highy) {
 	for (int i = lowx; i < highx; i++) {
 		for (int j = lowy; j < highy; j++) {
-			vector3 tempface = vector3(cam.forward.x + cam.right.x*(2 * sinOfF*i*pixelRatio - sinOfF)*RATIO + cam.up.x*(2 * sinOfF*j*piyelRatio - sinOfF), cam.forward.y + cam.right.y*(2 * sinOfF*i*pixelRatio - sinOfF)*RATIO + cam.up.y*(2 * sinOfF*j*piyelRatio - sinOfF), cam.forward.z + cam.right.z*(2 * sinOfF*i*pixelRatio - sinOfF)*RATIO + cam.up.x*(2 * sinOfF*j*piyelRatio - sinOfF));
+			vector3 tempface = cam.forward + cam.right*((2 * sinOfF*i*pixelRatio - sinOfF)*RATIO) + cam.up*(2 * sinOfF*j*piyelRatio - sinOfF);
 			planet *tempp = planets;
 			color tempc, minC = color(), white = color(255, 255, 255), black = color(0, 0, 0);
 			float errorMin = INF, errorTmp;
 
 			while (tempp != NULL) {
 				tempc = cam.rayTrace(tempface, tempp, errorTmp);
-				if (tempc == white && errorTmp == INF && minC == black) {
+				if (tempc == white && errorTmp == INF && errorMin == INF && minC == black) {
 					minC = white;
 				}
 				else if (errorTmp < errorMin) {
@@ -269,8 +302,55 @@ void multiThread(camera cam, float sinOfF, float pixelRatio, float piyelRatio, f
 		}
 	}
 }
-
+camera cam(vector3(0, 0, 2500));
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (action == GLFW_PRESS) {
+		
+		
+	}
+	if (true) {
+		vector3 temp;
+		vector3 back;
+		vector3 left;
+		vector3 down;
+		float angle = 0.01;
+		float aangle = 1 - angle;
+		switch (key) {
+		case GLFW_KEY_UP:
+			back = -cam.forward;
+			cam.up = (cam.up * aangle + back * angle).normalize();
+			cam.forward = (cam.up*cam.right).normalize();
+			break;
+		case GLFW_KEY_DOWN:
+			down = -cam.up;
+			cam.up = (cam.up * aangle + cam.forward * angle).normalize();
+			cam.forward = (cam.up*cam.right).normalize();
+			break;
+		case GLFW_KEY_LEFT:
+			left = -cam.right;
+			cam.right = (cam.right * aangle + cam.forward * angle).normalize();
+			cam.forward = (cam.up*cam.right).normalize();
+			break;
+		case GLFW_KEY_RIGHT:
+			back = -cam.forward;
+			cam.right = (cam.right * aangle + back * angle).normalize();
+			cam.forward = (cam.up*cam.right).normalize();
+			break;
+		case GLFW_KEY_W:
+			cam.pos += cam.forward * 25;
+			break;
+		case GLFW_KEY_S:
+			cam.pos -= cam.forward * 25;
+			break;
+		}
+	}
+		
+}
 int main() {
+
+
+
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
@@ -289,8 +369,8 @@ int main() {
 
 	glfwMakeContextCurrent(window);
 	glViewport(0, 0, WIDTH, HEIGHT);
-
-	camera cam(vector3(0, 0, 2500));
+	glfwSetKeyCallback(window, key_callback);
+	
 	sun = vector3(0, 0, 0);
 
 	planets = new planet(color(250, 100, 200), vector3(0, 500, 0), vector3(0, 0, 0), 100, 50, "P");
@@ -307,7 +387,7 @@ int main() {
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 
-		StartCounter();
+		//StartCounter();
 		thread Plocica1(multiThread, cam, sinOfF, pixelRatio, piyelRatio, RATIO, 0, 0, WIDTH/2, HEIGHT/2);
 		thread Plocica2(multiThread, cam, sinOfF, pixelRatio, piyelRatio, RATIO, WIDTH / 2, 0, WIDTH, HEIGHT / 2);
 		thread Plocica3(multiThread, cam, sinOfF, pixelRatio, piyelRatio, RATIO, 0, HEIGHT / 2, WIDTH / 2, HEIGHT);
@@ -343,10 +423,10 @@ int main() {
 
 		}*/
 
-		cout << GetCounter() << endl;
+		//cout << GetCounter() << endl;
 		// pixel matrix filled
 
-		/*planet *tempp = planets;
+		planet *tempp = planets;
 		float copet = 1000;
 		float copet2 = 0.1;
 		while (tempp != NULL) {
@@ -358,7 +438,7 @@ int main() {
 			}
 			tempp = tempp->next;
 		}
-*/
+
 
 
 		//planet *tempp = planets;
